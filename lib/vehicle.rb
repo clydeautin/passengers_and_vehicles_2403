12c2c70df2ef4e7af21099cf6e087dbce8507117
Clyde Autin <clyde.autin@gmail.com>
2024-04-03 10:00:35 -0700

    iteration_2 complete

--- lib/vehicle.rb
+++ lib/vehicle.rb
@@ -3,7 +3,8 @@ class Vehicle
     attr_reader :model_year,
                 :make,
                 :model,
-                :passengers
+                :passengers,
+                :adult_count
 
     def initialize(model_year, make, model)
         @model_year = model_year
@@ -11,6 +12,7 @@ class Vehicle
         @model = model
         @speeding = false
         @passengers = []
+        @adult_count = 0
     end
 
     def speeding?
@@ -23,5 +25,10 @@ class Vehicle
 
     def add_passenger(passenger)
         @passengers << passenger
+    end
+
+    def num_adults
+        num_adults = passengers.map { |passenger| passenger.adult? }
+        num_adults.count(true)
     end
 end
