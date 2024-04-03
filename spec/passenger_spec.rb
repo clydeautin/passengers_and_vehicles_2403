7adf27a5bd797d4ce30d54ccc4e61e6c7f188d35
Clyde Autin <clyde.autin@gmail.com>
2024-04-03 08:34:07 -0700

    test to determine if adult is created

--- spec/passenger_spec.rb
+++ spec/passenger_spec.rb
@@ -18,4 +18,12 @@ RSpec.describe Passenger do
         expect(charlie.name).to eq("Charlie")
     end
 
+    it 'can determine if a passenger is an adult' do
+        charlie = Passenger.new({"name" =>"Charlie", "age" => 18})
+        taylor = Passenger.new({"name" => "Taylor", "age" => 12})
+
+        expect(charlie.adult?).to eq(true)
+        expect(taylor.adult?).to eq(false)
+    end
+
 end
